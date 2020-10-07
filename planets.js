5e82ef04d00ea6fcf8862ad3c982817637ed1c9b
JoslyneLove <joslyne.lovelace@gmail.com>
2020-10-07 15:29:28 -0600

    Have ship following mouse

--- planets.js
+++ planets.js
@@ -1,95 +1,75 @@
 // Declare variables
 var canvas = document.getElementById("space");
-var ship = canvas.getContext("2d");
-var mer = canvas.getContext("2d");
-var ven = canvas.getContext("2d");
-var ear = canvas.getContext("2d");
-var mar = canvas.getContext("2d");
-var jup = canvas.getContext("2d");
-var sat = canvas.getContext("2d");
-var ura = canvas.getContext("2d");
-var nep = canvas.getContext("2d");
-var sun = canvas.getContext("2d");
-var shipImg = new Image();
-var merImg = new Image();
-var venImg = new Image();
-var earImg = new Image();
-var marImg = new Image();
-var jupImg = new Image();
-var satImg = new Image();
-var uraImg = new Image();
-var nepImg = new Image();
-var sunImg = new Image();
+var plan = canvas.getContext("2d");
+var ship = new Image();
+var mer = new Image();
+var ven = new Image();
+var ear = new Image();
+var mar = new Image();
+var jup = new Image();
+var sat = new Image();
+var ura = new Image();
+var nep = new Image();
+var sun = new Image();
 var xPos = 60;
 var yPos = 60;
 
 // Source of the image to be used
-shipImg.src = "./images/ufo.jpg";
-merImg.src = "./images/mercury_sm.jpg";
-venImg.src = "./images/venus_sm.jpg";
-earImg.src = "./images/earth_sm.jpg";
-marImg.src = "./images/mars_sm.jpg";
-jupImg.src = "./images/jupiter_sm.jpg";
-satImg.src = "./images/saturn_sm.jpg";
-uraImg.src = "./images/uranus_sm.jpg";
-nepImg.src = "./images/neptune_sm.jpg";
-sunImg.src = "./images/sun.jpg";
+ship.src = "./images/ufo.jpg";
+mer.src = "./images/mercury_sm.jpg";
+ven.src = "./images/venus_sm.jpg";
+ear.src = "./images/earth_sm.jpg";
+mar.src = "./images/mars_sm.jpg";
+jup.src = "./images/jupiter_sm.jpg";
+sat.src = "./images/saturn_sm.jpg";
+ura.src = "./images/uranus_sm.jpg";
+nep.src = "./images/neptune_sm.jpg";
+sun.src = "./images/sun.jpg";
 
 // Event to listen for keyboard activity
-document.addEventListener("keydown", checkKeyPressed, false);
+document.addEventListener("mousemove", shipMove, false);
 
-// Function of what to do when keyboard is pressed
-function checkKeyPressed(e) {
-  switch(e.keyCode) {
-	// Up key
-	case 38:
-	  yPos -= 5;
-	  if (yPos < 0) {
-		yPos = 0;
-	  }
-	  break;
-	// Down key
-	case 40:
-	  yPos += 5;
-	  if (yPos > canvas.height - shipImg.height) {
-		  yPos = canvas.height - shipImg.height;
-	  }
-	  break;
-	// Right key
-	case 39:
-	  xPos += 5;
-	  if (xPos > canvas.width - shipImg.width) {
-		xPos = canvas.width - shipImg.width;
-	  }
-	  break;
-	// Left key
-	case 37:
-	  xPos -= 5;
-	  if (xPos < 0) {
-		xPos = 0;
-      }
-	  break;
+function shipMove(event) {
+  var x = new Number();
+  var y = new Number();
+  var loc = false;
+
+  if (event.x != undefined && event.y != undefined) {
+    x = event.x;
+    y = event.y;
+  // Firefox method
+  } else {
+    x = event.clientX + document.body.scrollLeft +
+    document.documentElement.scrollLeft;
+    y = event.clientY + document.body.scrollTop +
+    document.documentElement.scrollTop;
   }
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  
+  xPos = x - (ship.width/2);
+  yPos = y - (ship.height/2);
+  
+  plan.clearRect(0, 0, canvas.width, canvas.height);
   drawPlanets();
 }
 
-
 // Function to draw planets on page load
-// Images need to be scaled down and positioned appropriately
 function drawPlanets() {
-  mer.drawImage(merImg, (canvas.width/2) + merImg.width - 110, (canvas.height/2) - (sunImg.height/2) - merImg.height);
-  ven.drawImage(venImg, (canvas.width/2) - (sunImg.width/2) - venImg.width, (canvas.height/2) + (venImg.height/2));
-  ear.drawImage(earImg, (canvas.width/2) + earImg.width, (canvas.height/2) + (earImg.height/2));
-  mar.drawImage(marImg, (canvas.width/2) - marImg.width - 150, 20);
-  jup.drawImage(jupImg, 0,  canvas.height - jupImg.height);
-  sat.drawImage(satImg, canvas.width - satImg.width, 0);
-  ura.drawImage(uraImg, 0, 0);
-  nep.drawImage(nepImg, canvas.width - nepImg.width, canvas.height - nepImg.height);
-  sun.drawImage(sunImg, (canvas.width/2) - (sunImg.width/2), (canvas.height/2) - (sunImg.height/2));
+  plan.drawImage(mer, (canvas.width/2) + mer.width - 110, (canvas.height/2) - (sun.height/2) - mer.height);
+  plan.drawImage(ven, (canvas.width/2) - (sun.width/2) - ven.width, (canvas.height/2) + (ven.height/2));
+  plan.drawImage(ear, (canvas.width/2) + ear.width, (canvas.height/2) + (ear.height/2));
+  plan.drawImage(mar, (canvas.width/2) - mar.width - 150, 20);
+  plan.drawImage(jup, 0,  canvas.height - jup.height);
+  plan.drawImage(sat, canvas.width - sat.width, 0);
+  plan.drawImage(ura, 0, 0);
+  plan.drawImage(nep, canvas.width - nep.width, canvas.height - nep.height);
+  plan.drawImage(sun, (canvas.width/2) - (sun.width/2), (canvas.height/2) - (sun.height/2));
   drawShip();
 }
 
 // Function to draw the ship
 function drawShip() {
-  ship.drawImage(shipImg, xPos, yPos);
+  plan.drawImage(ship, xPos, yPos);
 }
