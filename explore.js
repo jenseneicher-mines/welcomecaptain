252963c590891bf920a54cc52083827442c5c3d7
Jensen Eicher <jeicher@mymail.mines.edu>
2020-10-17 15:26:45 -0600

    added facts

--- explore.js
+++ explore.js
@@ -3,17 +3,20 @@ const shipSel = localStorage.getItem("shipSel");
 const score = localStorage.getItem("score");
 const planet = localStorage.getItem("planet");
 const planetsrc = localStorage.getItem("planetsrc");
-var canvas = document.getElementById("space");
-var surface = canvas.getContext("2d");
+const fact1 = localStorage.getItem("fact1");
+const fact2 = localStorage.getItem("fact2");
+const fact3 = localStorage.getItem("fact3");
+const fact4 = localStorage.getItem("fact4");
+console.log(planetsrc);
 
 // Display score
 document.getElementById("score").innerText = "Score: " + score + "/32";
 document.body.style.backgroundImage = "url(" + planetsrc + ")";
 
 var img = document.createElement("img");
-img.id = "I1"
+img.id = "I1";
 img.src = "./images/magnify.png";
-console.log(img)
+console.log(img);
 img.height = 100;
 img.width = 100;
 img.style.left = '85%';
@@ -22,20 +25,20 @@ img.style.position = 'absolute';
 document.body.appendChild(img);
 
 var img2 = document.createElement("img");
-img2.id = "I2"
+img2.id = "I2";
 img2.src = "./images/thermometer.png";
-console.log(img2)
+console.log(img2);
 img2.height = 100;
 img2.width = 100;
 img2.style.left = '90%';
-img2.style.top = '90%';
+img2.style.top = '85%';
 img2.style.position = 'absolute';
 document.body.appendChild(img2);
 
 var img3 = document.createElement("img");
-img3.id = "I3"
+img3.id = "I3";
 img3.src = "./images/clock.png";
-console.log(img3)
+console.log(img3);
 img3.height = 100;
 img3.width = 100;
 img3.style.position = 'absolute';
@@ -44,9 +47,9 @@ img3.style.top = '50%';
 document.body.appendChild(img3);
 
 var img4 = document.createElement("img");
-img4.id = "I4"
+img4.id = "I4";
 img4.src = "./images/tape.png";
-console.log(img4)
+console.log(img4);
 img4.height = 100;
 img4.width = 100;
 img4.style.left = '15%';
@@ -54,19 +57,46 @@ img4.style.top = '50%';
 img4.style.position = 'absolute';
 document.body.appendChild(img4);
 
+var text = document.createElement('text');
+text.type = "text";
+text.style.fontSize = "20px";
+text.style.fontWeight = "bold";
+text.style.backgroundColor = "lightgrey";
+text.style.borderRadius = "25px";
+text.style.paddingLeft = "25px";
+text.style.paddingRight = "25px";
+
+
+
 
 $('#I1').click(function() {
-    alert( "Magnifying Glass" );
+    text.textContent = fact1;
+    text.style.top = img.style.top;
+    text.style.left = img.style.left;
+    text.style.position = 'absolute';
+    document.body.appendChild(text);
 });
 
 $('#I2').click(function() {
-    alert( "thermometer" );
+    text.textContent = fact2;
+    text.style.top = img2.style.top;
+    text.style.left = img2.style.left;
+    text.style.position = 'absolute';
+    document.body.appendChild(text);
 });
 
 $('#I3').click(function() {
-    alert( "clock" );
+    text.textContent = fact3;
+    text.style.top = img3.style.top;
+    text.style.left = img3.style.left;
+    text.style.position = 'absolute';
+    document.body.appendChild(text);
 });
 
 $('#I4').click(function() {
-    alert( "tape measure" );
+    text.textContent = fact4;
+    text.style.top = img4.style.top;
+    text.style.left = img4.style.left;
+    text.style.position = 'absolute';
+    document.body.appendChild(text);
 });
--- planets.js
+++ planets.js
@@ -77,41 +77,73 @@ function gotoPlanet(e) {
 	  case "mer":
 	    localStorage.setItem("planet","mer");
 	    localStorage.setItem("planetsrc","./images/mercury_surface.jpg");
+	    localStorage.setItem("fact1","Interesting Fact, Mercury is the closest planet to the Sun");
+	    localStorage.setItem("fact2","Average Temperature, 800 deg F during the day, -290 deg F at night");
+	    localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 88 earth days");
+	    localStorage.setItem("fact4","Diameter, 2158.64 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "ven":
 	    localStorage.setItem("planet","ven");
 	    localStorage.setItem("planetsrc","./images/surface_venus.jpg");
+	    localStorage.setItem("fact1","Interesting Fact, Venus is the third brightest object in the Earth's sky after the Sun and Moon");
+	    localStorage.setItem("fact2","Average Temperature, 880 deg F");
+	    localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 225 earth days");
+	    localStorage.setItem("fact4","Diameter, 7521 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "ear":
 	    localStorage.setItem("planet","ear");
 	    localStorage.setItem("planetsrc","./images/earth_surface.jpg");
+        localStorage.setItem("fact1","Interesting Fact, Earth is the densest planet in the solar system at 5,513 kg/m3");
+        localStorage.setItem("fact2","Average Temperature, 61 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 365 earth days");
+        localStorage.setItem("fact4","Diameter, 7926 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "mar":
 	    localStorage.setItem("planet","mar");
 	    localStorage.setItem("planetsrc","./images/mars_sm.jpg");
+        localStorage.setItem("fact1","Interesting Fact, Mars has the tallest mountain known in the solar system at 68,897 ft high");
+        localStorage.setItem("fact2","Average Temperature, -20 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 687 earth");
+        localStorage.setItem("fact4","Diameter, 4212.275 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "jup":
 	    localStorage.setItem("planet","jup");
 	    localStorage.setItem("planetsrc","./images/jupiter_surface.jpg");
+        localStorage.setItem("fact1","Interesting Fact, Jupiter has the shortest day of the planets in the solar system, rotating once every 9 hours and 55 minutes");
+        localStorage.setItem("fact2","Average Temperature, -162 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 4,333 earth days");
+        localStorage.setItem("fact4","Diameter, 88,846 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "sat":
 	    localStorage.setItem("planet","sat");
 	    localStorage.setItem("planetsrc","./images/saturn_sm.jpg");
+        localStorage.setItem("fact1","Interesting Fact, Saturn has the fastest winds of any other planet in the solar system at 1,100 miles per hour");
+        localStorage.setItem("fact2","Average Temperature, -218 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 10,759 earth days");
+        localStorage.setItem("fact4","Diameter, 74,897.6 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "ura":
 	    localStorage.setItem("planet","ura");
 	    localStorage.setItem("planetsrc","./images/uranus_sm.jpg");
+	    localStorage.setItem("fact1","Interesting Fact, Uranus has 13 known rings, though these rings are quite narrow so they are less visible than Saturn's ring");
+        localStorage.setItem("fact2","Average Temperature, -320 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 30,687 earth days");
+        localStorage.setItem("fact4","Diameter, 31,518.43 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "nep":
 	    localStorage.setItem("planet","nep");
 	    localStorage.setItem("planetsrc","./images/neptune_sm.jpg");
+	    localStorage.setItem("fact1","Interesting Fact, Only one spacecraft has flown past Neptune, Voyager 2 in 1989");
+	    localStorage.setItem("fact2","Average Temperature, -331 deg F");
+        localStorage.setItem("fact3","Year length (Earth Days to travel around the sun), 60,190 earth days");
+        localStorage.setItem("fact4","Diameter, 30,598.8 miles");
 		window.open("explore.html","_self");
 		break;
 	  case "":
